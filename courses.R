f0bcf5562aa64f3cafadf4d812cf4fc038169433
David Dobrowski <dobrowski@gmail.com>
2020-02-12 14:16:58 -0800

    Added summarize function

--- courses.R
+++ courses.R
@@ -20,8 +20,15 @@ for (i in 12:17) {
 
 
 
-cs.courses <- assignments %>%
-    filter(str_detect(AssignmentSubject, "Computer"))
+# cs.courses <- assignments %>%
+#     filter(str_detect(AssignmentSubject, "Computer"))
+
+
+cs.courses <- read_xlsx(here("data", "Rodlist.xlsx"), col_names = FALSE) %>%
+    transmute(AssignmentCode = `...1`,
+              CourseName = `...2`)
+
+
 
 coursestaught.mry <- coursestaught %>% 
     filter(str_detect(CountyName,"MONTEREY"),
@@ -30,10 +37,31 @@ coursestaught.mry <- coursestaught %>%
     left_join(assignments, by = c("CourseCode" = "AssignmentCode"))
 
 
-write_csv(coursestaught.mry, "CS Courses in 17-18.csv")
+# write_csv(coursestaught.mry, "CS Courses in 17-18.csv")
+
+
+
+summary <- coursestaught.mry %>%
+    group_by(DistrictName,AcademicYear) %>%
+    mutate(enrolled = sum(as.numeric(Enrollment)) ,
+           n = n()) %>%
+    select(DistrictName, AcademicYear, enrolled, n) %>%
+    distinct() %>%
+    arrange(DistrictName, AcademicYear)
+
+write_csv(summary, "CS Courses by District by Year.csv")
+
+# 
+# summary <- coursestaught.mry %>%
+#     group_by(DistrictName,AcademicYear) %>%
+#     count()
+# 
 
 
 
 
+
+
+library(ipumsr)
 ddi <- read_ipums_ddi("usa_00001.xml")
 data <- read_ipums_micro(ddi)
