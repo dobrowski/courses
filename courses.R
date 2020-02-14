a700339c15f521a72f92c0be17b5892cdff2350d
David Dobrowski <dobrowski@gmail.com>
2020-02-14 09:16:27 -0800

    changed ingest to vroom

--- courses.R
+++ courses.R
@@ -3,23 +3,37 @@
 library(tidyverse)
 library(readxl)
 library(here)
+library(vroom)
+library(fs)
 
 
 ####  Load data  ------
 
 
 # Both files from https://www.cde.ca.gov/ds/sd/df/filesassign.asp
-assignments <- read_xlsx(here("data", "AssignmentCodes12On.xlsx"))
-coursestaught <- read_tsv(here("data", "CoursesTaught17.txt"), col_types = "ccccccccccccccccccc" ) %>%
-    filter(AcademicYear == "1")
+assignments <- read_xlsx(here("data", "AssignmentCodes12On.xlsx")) 
 
-for (i in 12:18) {
-    year <- read_tsv(here("data", paste0("CoursesTaught",i,".txt")), col_types = "ccccccccccccccccccc"  )
-    coursestaught <- bind_rows(coursestaught, year)
-}
+# 
+# coursestaught <- read_tsv(here("data", "CoursesTaught17.txt"), col_types = "ccccccccccccccccccc" ) %>%
+#     filter(AcademicYear == "1")
+# 
+# for (i in 12:18) {
+#     year <- read_tsv(here("data", paste0("CoursesTaught",i,".txt")), col_types = "ccccccccccccccccccc"  )
+#     coursestaught <- bind_rows(coursestaught, year)
+# }
+# 
+# coursestaught <- coursestaught %>%
+#     filter(str_detect(CountyName,"MONTEREY")) %>%
+#     left_join(assignments, by = c("CourseCode" = "AssignmentCode"))
 
-coursestaught <- coursestaught %>%
+
+    
+    
+
+coursestaught <- dir_ls( glob = "*/Course*txt", recurse = TRUE) %>%
+    vroom() %>% 
     filter(str_detect(CountyName,"MONTEREY")) %>%
+    mutate(CourseCode = as.character(CourseCode)) %>%
     left_join(assignments, by = c("CourseCode" = "AssignmentCode"))
 
 
