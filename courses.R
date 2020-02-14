3a01b70a149edb2f956ede963109e28055e90ae4
David Dobrowski <dobrowski@gmail.com>
2020-02-14 09:23:48 -0800

    deleting old code

--- courses.R
+++ courses.R
@@ -13,23 +13,6 @@ library(fs)
 # Both files from https://www.cde.ca.gov/ds/sd/df/filesassign.asp
 assignments <- read_xlsx(here("data", "AssignmentCodes12On.xlsx")) 
 
-# 
-# coursestaught <- read_tsv(here("data", "CoursesTaught17.txt"), col_types = "ccccccccccccccccccc" ) %>%
-#     filter(AcademicYear == "1")
-# 
-# for (i in 12:18) {
-#     year <- read_tsv(here("data", paste0("CoursesTaught",i,".txt")), col_types = "ccccccccccccccccccc"  )
-#     coursestaught <- bind_rows(coursestaught, year)
-# }
-# 
-# coursestaught <- coursestaught %>%
-#     filter(str_detect(CountyName,"MONTEREY")) %>%
-#     left_join(assignments, by = c("CourseCode" = "AssignmentCode"))
-
-
-    
-    
-
 coursestaught <- dir_ls( glob = "*/Course*txt", recurse = TRUE) %>%
     vroom() %>% 
     filter(str_detect(CountyName,"MONTEREY")) %>%
