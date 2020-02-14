618312ead3df9580212859dd8aa2f5910d4be25f
David Dobrowski <dobrowski@gmail.com>
2020-02-14 08:47:35 -0800

    Cleaning up old code

--- courses.R
+++ courses.R
@@ -23,55 +23,23 @@ coursestaught <- coursestaught %>%
     left_join(assignments, by = c("CourseCode" = "AssignmentCode"))
 
 
-# cs.courses <- assignments %>%
-#     filter(str_detect(AssignmentSubject, "Computer"))
+### Examples of course lists -----
 
 
 cs.courses <- read_xlsx(here("data", "Rodlist.xlsx"), col_names = FALSE) %>%
     transmute(AssignmentCode = `...1`,
               CourseName = `...2`)
 
+
+# Do you have data sources that will allow you to look up the number of schools in MCOE that are providing arts programming?
+# Let's see all of the schools that have art programs, theater/dance, music, dance, fashion and interior design, arts media and entertainment as well as 
+# 4605, 4606,4607,4617, 4635 & 2455
+
 art <- assignments %>% 
     filter(str_detect(AssignmentSubject,"Art|Dance|Drama|Fashion|Music") | AssignmentCode %in% c(4605, 4606,4607,4635,2455)  ) %>%
     filter(!str_detect(AssignmentSubject,"English"))
 
-
-
-# art programs, theater/dance, music, dance, fashion and interior design, arts media and entertainment as well as 
-# 4605, 4606,4607,4617, 4635 & 2455
-
-
-
-coursestaught.mry <- coursestaught %>% 
-    filter(str_detect(CountyName,"MONTEREY"),
-           CourseCode %in% art$AssignmentCode,
-           AcademicYear == "1819") %>%
-#    mutate(CourseCode = as.character(CourseCode)) %>%
-    left_join(assignments, by = c("CourseCode" = "AssignmentCode"))
-
-
-# write_csv(coursestaught.mry, "CS Courses in 17-18.csv")
-
-
-
-summary <- coursestaught.mry %>%
-    group_by(DistrictName, SchoolName, AcademicYear) %>%
-    mutate(NumberEnrolled = sum(as.numeric(Enrollment)) ,
-           NumberCourses = n()) %>%
-    select(DistrictName, SchoolName, AcademicYear, NumberEnrolled, NumberCourses) %>%
-    distinct() %>%
-    arrange(DistrictName, AcademicYear)
-
-write_csv(summary, "Art Courses by District by Year.csv")
-
-# 
-# summary <- coursestaught.mry %>%
-#     group_by(DistrictName,AcademicYear) %>%
-#     count()
-# 
-
-
-
+### Function -----
 
 courses.summary <- function(courselist) {
     
@@ -85,4 +53,12 @@ courses.summary <- function(courselist) {
         arrange(DistrictName, SchoolName ,AcademicYear)
 }
 
+
+###  Example execution -----
+
 courses.summary(art$AssignmentCode)
+
+write_csv(summary, "Art Courses by District by Year.csv")
+
+
+### End ------
